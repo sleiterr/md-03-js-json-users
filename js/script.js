a1564b401fd2c8ab2474f8788abfb90a7ecd7a19
Oleg <sleite4r@gmail.com>
2025-01-30 22:09:26 +0100

    create userstemplete, Add user information display component with dynamic data binding

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 const usersBox = document.querySelector(".users-box");
 
+const userstemplete = (post) => {
+  return `
+    <ul class='users-list'>
+      <li class="list-item">
+        <div class="img-box">
+         <img src="${post.src}" alt="${post.userName}" class="users-img" />
+        </div>
+        <div class="users-info">
+         <h4 class="name-title">${post.title}</h4>
+         <p class="item-subtitle">${post.nickname}</p>
+         <a href="mailto:${post.email}" class="users-email"
+            >${post.email}</a>
+         <a href="#" class="users-link">Se website</a>
+        </div>
+        </li>
+    </ul>
+    `;
+};
+
 
