91e2194427094d713516518fbb1e11d3d1b1187d
Oleg <sleite4r@gmail.com>
2025-01-31 18:16:31 +0100

    create bttn grid-list, add (e). create list conteiner

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const usersBox = document.querySelector(".users-box");
+const listContainer = document.querySelector(".list-container");
 
-const userstemplete = (post) => {
-  return `
+const btnGrid = document.querySelector(".btn-grid");
+const btnList = document.querySelector(".btn-list");
+
+/* template functions */
+const postTemplateGrid = (post) => {
+  return ` 
     <ul class='users-list'>
       <li class="list-item">
         <div class="img-box">
@@ -15,6 +20,24 @@ const userstemplete = (post) => {
          <a href="#" class="users-link">Se website</a>
         </div>
         </li>
+    </ul>
+    `;
+};
+
+const postTemplateList = (post) => {
+  return `
+    <ul class='users-list'>
+      <li class="list-users">
+        <div class="image-box">
+         <img src="${post.src}" alt="${post.userName}" class="users-image" />
+        </div>
+        <div class="user-info">
+         <h4 class="list-title">${post.userName}</h4>
+         <p class="item-subtitle">${post.nickname}</p>
+        </div>
+        <a href="mailto:${post.email}" class="list-email">${post.email}</a>
+        <a href="#" class="users-link">Se website</a>
+      </li>
     </ul>
     `;
 };
@@ -34,9 +57,26 @@ const renderPost = async () => {
   const posts = await fetchPosts();
 
   posts.forEach((post) => {
-    usersBox.innerHTML += userstemplete(post);
+    usersBox.innerHTML += postTemplateGrid(post);
   });
-  console.log(posts);
+
+  btnGrid.addEventListener("click", () => {
+    listContainer.innerHTML = "";
+    usersBox.innerHTML = "";
+    posts.forEach((post) => {
+      usersBox.innerHTML += postTemplateGrid(post);
+    });
+  });
+
+  btnList.addEventListener("click", () => {
+    listContainer.innerHTML = "";
+    usersBox.innerHTML = "";
+    posts.forEach((post) => {
+      listContainer.innerHTML += postTemplateList(post);
+    });
+  });
+
+  //   console.log(posts);
 };
 
 renderPost();
